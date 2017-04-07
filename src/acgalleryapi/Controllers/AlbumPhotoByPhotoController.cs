27d8a84453ba9cc315b92e1c7a8b5333c4bcb6ae
Alva Chien <alvachien@163.com>
2017-04-07 19:41:02 +0800

    Recent changes

---
+++ src/acgalleryapi/Controllers/AlbumPhotoByPhotoController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Authorization;
+using acgalleryapi.ViewModels;
+
+namespace acgalleryapi.Controllers
+{
+    [Route("api/albumphotobyphoto")]
+    public class AlbumPhotoByPhotoController : Controller
+    {
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create([FromBody]AlbumPhotoByPhotoViewModel vm)
+        {
+            if (vm == null)
+            {
+                return BadRequest("No data is inputted");
+            }
+
+            if (TryValidateModel(vm))
+            {
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            var usrName = User.FindFirst(c => c.Type == "sub").Value;
+            var scopeStr = User.FindFirst(c => c.Type == "GalleryAlbumChange").Value;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Startup.DBConnectionString))
+                {
+                    await conn.OpenAsync();
+
+                    String albumList = String.Join(",", vm.AlbumIDList);
+
+                    String queryString = @"SELECT [AlbumID]
+                          ,[Title]
+                          ,[Desp]
+                          ,[CreatedBy]
+                          ,[CreateAt]
+                          ,[IsPublic]
+                          ,[AccessCode]
+                      FROM [dbo].[Album]
+                      WHERE [AlbumID] IN ( " + albumList + " )";
+
+                    SqlCommand cmd = new SqlCommand(queryString, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        String strCreatedBy = String.Empty;
+                        while (reader.Read())
+                        {
+                            if (!reader.IsDBNull(3))
+                                strCreatedBy = reader.GetString(3);
+
+                            if (String.CompareOrdinal(scopeStr, "All") == 0)
+                            {
+                                // Do nothing
+                            }
+                            else if (String.CompareOrdinal(scopeStr, "OnlyOwner") == 0)
+                            {
+                                if (String.CompareOrdinal(strCreatedBy, usrName) != 0)
+                                {
+                                    return Unauthorized();
+                                }
+                                else
+                                {
+                                    // Do nothing
+                                }
+                            }
+                            else
+                            {
+                                return BadRequest();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+
+                    reader.Dispose();
+                    reader = null;
+                    cmd.Dispose();
+                    cmd = null;
+
+                    List<String> listCmds = new List<string>();
+                    // Delete the records from album                    
+                    String cmdText = @"DELETE FROM [dbo].[AlbumPhoto] WHERE [PhotoID] = N'" + vm.PhotoID + "'";
+                    listCmds.Add(cmdText);
+
+                    foreach (Int32 aid in vm.AlbumIDList)
+                    {
+                        cmdText = @"INSERT INTO [dbo].[AlbumPhoto]
+                               ([AlbumID]
+                               ,[PhotoID])
+                         VALUES(" + aid.ToString()
+                        + @", N'" + vm.PhotoID
+                         + @"')";
+                        listCmds.Add(cmdText);
+                    }
+                    String allQueries = String.Join(";", listCmds);
+
+                    cmd = new SqlCommand(allQueries, conn);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+                return StatusCode(500, exp.Message);
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
---
+++ src/acgalleryapi/Controllers/AlbumPhotoLinkController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Authorization;
+using acgalleryapi.ViewModels;
+
+namespace acgalleryapi.Controllers
+{
+    [Route("api/albumphotolink")]
+    public class AlbumPhotoLinkController : Controller
+    {
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create([FromBody]AlbumPhotoLinkViewModel vm)
+        {
+            if (vm == null)
+            {
+                return BadRequest("No data is inputted");
+            }
+
+            if (TryValidateModel(vm))
+            {
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            var usrName = User.FindFirst(c => c.Type == "sub").Value;
+            var scopeStr = User.FindFirst(c => c.Type == "GalleryAlbumChange").Value;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Startup.DBConnectionString))
+                {
+                    await conn.OpenAsync();
+
+                    String queryString = @"SELECT [AlbumID]
+                          ,[Title]
+                          ,[Desp]
+                          ,[CreatedBy]
+                          ,[CreateAt]
+                          ,[IsPublic]
+                          ,[AccessCode]
+                      FROM [dbo].[Album]
+                      WHERE [AlbumID] = " + vm.AlbumID.ToString();
+
+                    SqlCommand cmd = new SqlCommand(queryString, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        String strCreatedBy = String.Empty;
+                        while (reader.Read())
+                        {
+                            if (!reader.IsDBNull(3))
+                                strCreatedBy = reader.GetString(3);
+
+                            if (String.CompareOrdinal(scopeStr, "All") == 0)
+                            {
+                                // Do nothing
+                            }
+                            else if (String.CompareOrdinal(scopeStr, "OnlyOwner") == 0)
+                            {
+                                if (String.CompareOrdinal(strCreatedBy, usrName) != 0)
+                                {
+                                    return Unauthorized();
+                                }
+                                else
+                                {
+                                    // Do nothing
+                                }
+                            }
+                            else
+                            {
+                                return BadRequest();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+
+                    reader.Dispose();
+                    reader = null;
+                    cmd.Dispose();
+                    cmd = null;
+
+                    // Delete the records from album
+                    String cmdText = @"INSERT INTO [dbo].[AlbumPhoto] ([AlbumID], [PhotoID]) VALUES( " + vm.AlbumID.ToString() + ", N'" + vm.PhotoID + "');";
+                    cmd = new SqlCommand(cmdText, conn);
+                    await cmd.ExecuteNonQueryAsync();
+
+                    cmd.Dispose();
+                    cmd = null;
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+                return StatusCode(500, exp.Message);
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
