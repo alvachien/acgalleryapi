31fab38ebbce1a1100ff477deca31a8c2a62ec2a
Alva Chien <alvachien@163.com>
2017-04-11 12:25:35 +0800

    Delete the linkage now work

--- src/acgalleryapi/Controllers/AlbumPhotoLinkController.cs
+++ src/acgalleryapi/Controllers/AlbumPhotoLinkController.cs
@@ -106,5 +106,95 @@ namespace acgalleryapi.Controllers
 
             return new EmptyResult();
         }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> Delete([FromBody]AlbumPhotoLinkViewModel vm)
+        {
+            if (vm == null)
+            {
+                return BadRequest("No data is inputted");
+            }
+
+            if (TryValidateModel(vm))
+            {
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            var usrName = User.FindFirst(c => c.Type == "sub").Value;
+            var scopeStr = User.FindFirst(c => c.Type == "GalleryAlbumChange").Value;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Startup.DBConnectionString))
+                {
+                    await conn.OpenAsync();
+
+                    String queryString = @"SELECT [CreatedBy]
+                      FROM [dbo].[Album]
+                      WHERE [AlbumID] = " + vm.AlbumID.ToString();
+
+                    SqlCommand cmd = new SqlCommand(queryString, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        String strCreatedBy = String.Empty;
+                        while (reader.Read())
+                        {
+                            if (!reader.IsDBNull(0))
+                                strCreatedBy = reader.GetString(0);
+
+                            if (String.CompareOrdinal(scopeStr, "All") == 0)
+                            {
+                                // Do nothing
+                            }
+                            else if (String.CompareOrdinal(scopeStr, "OnlyOwner") == 0)
+                            {
+                                if (String.CompareOrdinal(strCreatedBy, usrName) != 0)
+                                {
+                                    return Unauthorized();
+                                }
+                                else
+                                {
+                                    // Do nothing
+                                }
+                            }
+                            else
+                            {
+                                return BadRequest();
+                            }
+                            break; // Only one records!
+                        }
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+
+                    reader.Dispose();
+                    reader = null;
+                    cmd.Dispose();
+                    cmd = null;
+
+                    // Delete the records from album
+                    String cmdText = @"DELETE FROM [dbo].[AlbumPhoto] ([AlbumID], [PhotoID]) VALUES( " + vm.AlbumID.ToString() + ", N'" + vm.PhotoID + "');";
+                    cmd = new SqlCommand(cmdText, conn);
+                    await cmd.ExecuteNonQueryAsync();
+
+                    cmd.Dispose();
+                    cmd = null;
+                }
+            }
+            catch (Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine(exp.Message);
+                return StatusCode(500, exp.Message);
+            }
+
+            return new EmptyResult();
+        }
     }
 }
