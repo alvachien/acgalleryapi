f3eeb312319def27730362d1166a7347f2a2b5fd
Alva Chien <alvachien@163.com>
2017-10-13 23:25:25 +0800

    Update the API to ASP.NET Core 2

--- src/acgalleryapi/Startup.cs
+++ src/acgalleryapi/Startup.cs
@@ -14,32 +14,22 @@ namespace acgalleryapi
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-#if DEBUG
-            env.EnvironmentName = "Development";
-#else
-            env.EnvironmentName = "Production";
-#endif
-
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
+
         internal static String DBConnectionString { get; private set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
 
             // Add framework services.
             services.AddMvcCore()
+                .AddAuthorization()
                 .AddJsonFormatters()
                 .AddAuthorization(
                     options => {
