c7e9b6e61fd42bd8b234cbadcb76e99ae5734c28
Alva Chien <alvachien@163.com>
2018-01-02 21:05:38 +0800

    Add wakeup API

---
+++ src/acgalleryapi/Controllers/WakeupController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace acgalleryapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Wakeup")]
+    public class WakeupController : Controller
+    {
+        // GET: api/Wakeup
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "wake", "up" };
+        }
+    }
+}
