b171e65db94e8a4f9399d29a6544281f52e42241
Alva Chien <alvachien@163.com>
2018-06-05 22:02:06 +0800

    Continues the coding

--- src/acgalleryapi/Controllers/PhotoTagController.cs
+++ src/acgalleryapi/Controllers/PhotoTagController.cs
@@ -15,6 +15,9 @@ namespace acgalleryapi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            string strSql = @"SELECT [PhotoID]
+      ,[Tag]
+  FROM [dbo].[PhotoTag]";
             return new string[] { "value1", "value2" };
         }
 
@@ -29,18 +32,30 @@ namespace acgalleryapi.Controllers
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            String strSql = @"INSERT INTO [dbo].[PhotoTag]
+           ([PhotoID]
+           ,[Tag])
+     VALUES
+           (<PhotoID, nvarchar(40),>
+           ,<Tag, nvarchar(50),>)";
         }
         
         // PUT: api/PhotoTag/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            string strSql = @"UPDATE [dbo].[PhotoTag]
+   SET [PhotoID] = <PhotoID, nvarchar(40),>
+      ,[Tag] = <Tag, nvarchar(50),>
+ WHERE <Search Conditions,,>";
         }
         
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            string strSql = @"DELETE FROM [dbo].[PhotoTag]
+      WHERE <Search Conditions,,>";
         }
     }
 }
--- src/acgalleryapi/Controllers/UserDetailController.cs
+++ src/acgalleryapi/Controllers/UserDetailController.cs
@@ -15,11 +15,23 @@ namespace acgalleryapi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            String strSql = @"SELECT [UserID]
+                          ,[DisplayAs]
+                          ,[UploadFileMinSize]
+                          ,[UploadFileMaxSize]
+                          ,[AlbumCreate]
+                          ,[AlbumChange]
+                          ,[AlbumDelete]
+                          ,[PhotoUpload]
+                          ,[PhotoChange]
+                          ,[PhotoDelete]
+                          ,[AlbumRead]
+                      FROM [dbo].[UserDetail];";
             return new string[] { "value1", "value2" };
         }
 
         // GET: api/UserDetail/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
@@ -29,18 +41,57 @@ namespace acgalleryapi.Controllers
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            String strSql = @"INSERT INTO [dbo].[UserDetail]
+           ([UserID]
+           ,[DisplayAs]
+           ,[UploadFileMinSize]
+           ,[UploadFileMaxSize]
+           ,[AlbumCreate]
+           ,[AlbumChange]
+           ,[AlbumDelete]
+           ,[PhotoUpload]
+           ,[PhotoChange]
+           ,[PhotoDelete]
+           ,[AlbumRead])
+     VALUES
+           (<UserID, nvarchar(50),>
+           ,<DisplayAs, nvarchar(50),>
+           ,<UploadFileMinSize, int,>
+           ,<UploadFileMaxSize, int,>
+           ,<AlbumCreate, bit,>
+           ,<AlbumChange, tinyint,>
+           ,<AlbumDelete, tinyint,>
+           ,<PhotoUpload, bit,>
+           ,<PhotoChange, tinyint,>
+           ,<PhotoDelete, tinyint,>
+           ,<AlbumRead, tinyint,>)";
         }
         
         // PUT: api/UserDetail/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            string strSql = @"UPDATE [dbo].[UserDetail]
+   SET [UserID] = <UserID, nvarchar(50),>
+      ,[DisplayAs] = <DisplayAs, nvarchar(50),>
+      ,[UploadFileMinSize] = <UploadFileMinSize, int,>
+      ,[UploadFileMaxSize] = <UploadFileMaxSize, int,>
+      ,[AlbumCreate] = <AlbumCreate, bit,>
+      ,[AlbumChange] = <AlbumChange, tinyint,>
+      ,[AlbumDelete] = <AlbumDelete, tinyint,>
+      ,[PhotoUpload] = <PhotoUpload, bit,>
+      ,[PhotoChange] = <PhotoChange, tinyint,>
+      ,[PhotoDelete] = <PhotoDelete, tinyint,>
+      ,[AlbumRead] = <AlbumRead, tinyint,>
+ WHERE <Search Conditions,,>";
         }
         
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            string strSql = @"DELETE FROM [dbo].[UserDetail]
+      WHERE <Search Conditions,,>";
         }
     }
 }
