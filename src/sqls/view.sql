7b0f29610dd3cfb4a50a05b1b1c6c27249cdc54d
Alva Chien <alvachien@live.com>
2021-04-06 23:53:31 +0800

    Publish to Ali

--- src/sqls/view.sql
+++ src/sqls/view.sql
@@ -2,4 +2,5 @@
 CREATE VIEW [dbo].[PhotoView] AS ( select a.*, b.Tags
 	from Photo as a
 	left outer join ( select PhotoID, STRING_AGG(Tag, ',') as Tags from PhotoTag group by PhotoID ) as b
-	on a.PhotoID = b.PhotoID );
+	on a.PhotoID = b.PhotoID
+		where a.IsPublic = 1);
